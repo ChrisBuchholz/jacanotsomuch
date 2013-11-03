e8d5cf0e30d922ea7e5a60df1394f601c84a7ec3
Christoffer Buchholz <christoffer.buchholz@gmail.com>
2013-11-03 18:02:30 +0100

    add better git info

--- README.md
+++ README.md
@@ -5,4 +5,4 @@ theme that is distributed with oh-my-fish.
 
 The only difference is that I have removed the directory size and the date
 from the prompt so that it now only show user, host, directory and
-git information.
+(a bit more) git information.
--- fish_prompt.fish
+++ fish_prompt.fish
@@ -1,27 +1,44 @@
 # name: jacanotsomuch
-# tweaked version of the jacaetevha that is distributed with oh-my-fish
+# tweaked version of the jacaetevha theme
+
+function parse_git_dirty
+    if test (git status 2> /dev/null ^&1 | tail -n1) != "nothing to commit, working directory clean"
+        echo (set_color red)
+    else
+        echo (set_color brown)
+    end
+end
+
+function parse_git_branch
+    set -l branch (git branch 2> /dev/null | sed -e '/^[^*]/d' -e 's/* \(.*\)/\1/')
+    parse_git_dirty
+    printf '(%s)' $branch
+    set_color normal
+end
+
 function fish_prompt
-  set_color yellow
-  printf '%s' (whoami)
-  set_color normal
-  printf ' at '
+    # Line 1
+    set_color yellow
+    printf '%s' (whoami)
+    set_color normal
+    printf ' at '
 
-  set_color magenta
-  printf '%s' (hostname|cut -d . -f 1)
-  set_color normal
-  printf ' in '
+    set_color magenta
+    printf '%s' (hostname|cut -d . -f 1)
+    set_color normal
+    printf ' in '
 
-  set_color $fish_color_cwd
-  printf '%s ' (prompt_pwd)
-  set_color normal
-  printf '%s' (__fish_git_prompt)
+    set_color $fish_color_cwd
+    printf '%s ' (prompt_pwd)
+    set_color normal
 
-  # Line 2
-  echo
-  if test $VIRTUAL_ENV
-      printf "(%s) " (set_color blue)(basename $VIRTUAL_ENV)(set_color normal)
-  end
-  printf '↪  '
-  set_color normal
+    if test -z (git branch -quiet 2>| awk '/fatal:/ {print "no git"}')
+        printf '%s' (parse_git_branch)
+    end
+
+    # Line 2
+    echo
+    printf '↪  '
+    set_color normal
 end
 
